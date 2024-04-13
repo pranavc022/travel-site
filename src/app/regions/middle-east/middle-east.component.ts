09d9e97278235ece46aeb9a0c089ec6ee1dfaeec
Pranav C <97249097+pranavc022@users.noreply.github.com>
2024-04-13 18:24:31 +0530

    destinations, filter added and functional

--- src/app/regions/middle-east/middle-east.component.ts
+++ src/app/regions/middle-east/middle-east.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TourPackagesService } from '../../services/tour-packages.service';
+import { TourPackage } from '../../model/tour-package';
 
 @Component({
   selector: 'app-middle-east',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './middle-east.component.css'
 })
 export class MiddleEastComponent {
+  tourPackages: TourPackage[] = [];
+  adventureTypes = ['Hill Stations', 'Desert Safari', 'Water Sports', 'Historical', 'Wonders'];
+  selectedAdventureType: string;
+  costPerPersonRange: [number, number] = [1500, 3000];
 
+  constructor(private tourPackagesService: TourPackagesService) {}
+
+  ngOnInit() {
+    this.fetchTourPackages();
+  }
+
+  fetchTourPackages() {
+    this.tourPackages = this.tourPackagesService.getTourPackages('3', this.costPerPersonRange);
+  }
+
+  onAdventureTypeSelect() {
+    this.fetchTourPackages();
+  }
+
+  onCostRangeChange() {
+    this.fetchTourPackages();
+  }
+  clearFilters() {
+    this.selectedAdventureType = '';
+    this.costPerPersonRange = [1500, 3000];
+    this.fetchTourPackages();
+  }
 }
