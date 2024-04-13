845880581740e31236a9f26aa561a3504d581a00
lep_13 <lepakshi1304@gmail.com>
2024-04-13 14:32:36 +0530

    changes made by leps

--- src/app/country-carousel/country-carousel.component.ts
+++ src/app/country-carousel/country-carousel.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-country-carousel',
   templateUrl: './country-carousel.component.html',
-  styleUrl: './country-carousel.component.css'
+  styleUrls: ['./country-carousel.component.css'] 
 })
-export class CountryCarouselComponent implements OnInit, OnDestroy{
-  images = [
+export class CountryCarouselComponent implements OnInit, OnDestroy {
+  images: string[] = [
     'https://static.vecteezy.com/system/resources/previews/001/116/378/large_2x/beijing-great-wall-of-china-photo.jpg', 
-    'https://www.hdwallpapers.in/download/taj_mahal_agra_india_4k-1440x2560.jpg', 
+    'https://th.bing.com/th/id/R.58d7487a920013a0117c85cded1023a9?rik=UB486bFnTaXTWQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fb%2f4%2f6%2f1126902-beautiful-hd-wallpaper-of-india-2800x2100-ios.jpg&ehk=OiEuJbbyEj5jqSFHgjTgDUm3FE8vntthC3dtTifmaPI%3d&risl=&pid=ImgRaw&r=0', 
     'https://th.bing.com/th/id/OIP.3nV7uthpIj9rBZojEBArfAHaNK?rs=1&pid=ImgDetMain',
     'https://r1.ilikewallpaper.net/iphone-wallpapers/download/98355/Venice-HD-Backgrounds-iphone-wallpaper-ilikewallpaper_com.jpg',
     'https://www.hdwallpapers.in/download/dubai_night_cityscape_4k-1080x1920.jpg',
@@ -16,7 +16,8 @@ export class CountryCarouselComponent implements OnInit, OnDestroy{
     'https://i.pinimg.com/originals/1e/37/81/1e3781df3bff2dd3ea0a14dde18479fc.jpg',
     'https://www.tourist-destinations.com/wp-content/uploads/2014/04/c82c37ac57432f7d338f11bc8076e6f6.jpg'
   ];
-  currentImageIndex = 0;
+  
+  currentImageIndex: number = 0;
   autoSlideInterval: any;
 
   ngOnInit(): void {
@@ -32,7 +33,7 @@ export class CountryCarouselComponent implements OnInit, OnDestroy{
   startAutoSlide(): void {
     this.autoSlideInterval = setInterval(() => {
       this.nextImage();
-    }, 5000); // Images will change every 2 seconds
+    }, 5000); // Images will change every 5 seconds
   }
 
   nextImage(): void {
@@ -42,5 +43,4 @@ export class CountryCarouselComponent implements OnInit, OnDestroy{
   previousImage(): void {
     this.currentImageIndex = (this.currentImageIndex - 1 + this.images.length) % this.images.length;
   }
-
 }
