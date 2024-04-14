4ecb210bcbdc12a729a0e3e6be4ee890e10a7bcc
lep_13 <lepakshi1304@gmail.com>
2024-04-14 13:12:49 +0530

    Update carousel component styles and script -leps

--- src/app/country-carousel/country-carousel.component.ts
+++ src/app/country-carousel/country-carousel.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class CountryCarouselComponent implements OnInit, OnDestroy {
   images: string[] = [
     'https://static.vecteezy.com/system/resources/previews/001/116/378/large_2x/beijing-great-wall-of-china-photo.jpg', 
-    'https://th.bing.com/th/id/R.58d7487a920013a0117c85cded1023a9?rik=UB486bFnTaXTWQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fb%2f4%2f6%2f1126902-beautiful-hd-wallpaper-of-india-2800x2100-ios.jpg&ehk=OiEuJbbyEj5jqSFHgjTgDUm3FE8vntthC3dtTifmaPI%3d&risl=&pid=ImgRaw&r=0', 
-    'https://th.bing.com/th/id/OIP.3nV7uthpIj9rBZojEBArfAHaNK?rs=1&pid=ImgDetMain',
-    'https://r1.ilikewallpaper.net/iphone-wallpapers/download/98355/Venice-HD-Backgrounds-iphone-wallpaper-ilikewallpaper_com.jpg',
-    'https://www.hdwallpapers.in/download/dubai_night_cityscape_4k-1080x1920.jpg',
-    'https://th.bing.com/th/id/R.7c410cbc521962574e9126f4bf0c61f1?rik=dk%2bRMrGRX1jQ8Q&riu=http%3a%2f%2fqatarairways.com%2fcontent%2fdam%2fimages%2frenditions%2fvertical%2fdestinations%2fsaudi-arabia%2friyadh%2fv-riyadh-skyline2.jpg&ehk=70hpgmVxDF5n7Gk9z5sdbgnm%2bfsw%2fWMnIsbDEvSVAiM%3d&risl=&pid=ImgRaw&r=0',
-    'https://i.pinimg.com/originals/1e/37/81/1e3781df3bff2dd3ea0a14dde18479fc.jpg',
-    'https://www.tourist-destinations.com/wp-content/uploads/2014/04/c82c37ac57432f7d338f11bc8076e6f6.jpg'
+    'https://wallpaperaccess.com/full/8231407.jpg', 
+    'https://i.pinimg.com/originals/09/0c/6e/090c6e54764437fc0bc456c3acea2a25.jpg',
+    'https://r1.ilikewallpaper.net/ipad-wallpapers/download/16589/Venice-Italy-ipad-wallpaper-ilikewallpaper_com.jpg',
+    'https://www.wakdxbllc.com/img/duba.jpeg',
+    'https://th.bing.com/th/id/R.055dc755db81efb08c2be1a73c9aa1ed?rik=3tU1k9qpKzouew&riu=http%3a%2f%2fim.rediff.com%2fmoney%2f2014%2fmar%2f10cheapest-cities2.jpg&ehk=clN%2fe2caRc81FG7u%2fgQ4idXLRn6%2b3LM%2fgsVX8e7M6dY%3d&risl=&pid=ImgRaw&r=0',
+    'https://w0.peakpx.com/wallpaper/51/723/HD-wallpaper-statue-of-liberty-clouds-liberty-new-york-sky-statue.jpg',
+    'https://i.pinimg.com/originals/6f/bc/b6/6fbcb6c3b964126a9cb11356df3695e3.jpg'
   ];
   
   currentImageIndex: number = 0;
